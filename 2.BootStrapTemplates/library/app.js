0b95330852ffd21c7716832b2083fe077d114a2f
Srinadh K <Srinadh K>
2016-04-27 00:38:32 -0500

    added bootstrap template

---
+++ 2.BootStrapTemplates/library/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var app = express();
+
+var port = 5000;
+
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
+
+app.get('/books', function(req, res){
+    res.send('Hello Books');
+});
+
+app.listen(port, function(err){
+    console.log('Running Server on Port- ' +port);
+});
